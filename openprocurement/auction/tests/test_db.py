019bd05632199e2cd0f00ed116d2baad4e376ae6
godsdog <an.andrusyk@gmail.com>
2018-03-14 12:52:17 +0200

    Simplify test_db.py

--- openprocurement/auction/tests/test_db.py
+++ openprocurement/auction/tests/test_db.py
@@ -13,85 +13,93 @@ class TestTemplateViews(object):
     @pytest.mark.parametrize(
         'save_doc', [test_public_document], indirect=['save_doc'])
     def test_chronograph_view(self, db2, save_doc):
-        for data in db2.view('chronograph/start_date').rows:
-            assert data.get('value') == {u'start': save_doc['stages'][0]['start'],
-                                         u'procurementMethodType': u'',
-                                         u'auction_type': save_doc['auction_type'],
-                                         u'mode': u'', u'api_version': save_doc['TENDERS_API_VERSION']}
+        data = db2.view('chronograph/start_date').rows
+        assert len(data) == 1
+        assert data[0].get('value') == {u'start': save_doc['stages'][0]['start'],
+                                        u'procurementMethodType': u'',
+                                        u'auction_type': save_doc['auction_type'],
+                                        u'mode': u'', u'api_version': save_doc['TENDERS_API_VERSION']}
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document_with_mode], indirect=['save_doc'])
     def test_chronograph_view_mode(self, db2, save_doc):
-        for data in db2.view('chronograph/start_date').rows:
-            assert data.get('value') == {u'start': save_doc['stages'][0]['start'],
-                                         u'procurementMethodType': u'',
-                                         u'auction_type': save_doc['auction_type'],
-                                         u'mode': save_doc['mode'], u'api_version': save_doc['TENDERS_API_VERSION']}
+        data = db2.view('chronograph/start_date').rows
+        assert len(data) == 1
+        assert data[0].get('value') == {u'start': save_doc['stages'][0]['start'],
+                                        u'procurementMethodType': u'',
+                                        u'auction_type': save_doc['auction_type'],
+                                        u'mode': save_doc['mode'], u'api_version': save_doc['TENDERS_API_VERSION']}
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document_no_api_version], indirect=['save_doc'])
     def test_chronograph_view_api_version(self, db2, save_doc):
-        for data in db2.view('chronograph/start_date').rows:
-            assert data.get('value') == {u'start': save_doc['stages'][0]['start'],
-                                         u'procurementMethodType': u'',
-                                         u'auction_type': save_doc['auction_type'],
-                                         u'mode': u'', u'api_version': None}
+        data = db2.view('chronograph/start_date').rows
+        assert len(data) == 1
+        assert data[0].get('value') == {u'start': save_doc['stages'][0]['start'],
+                                        u'procurementMethodType': u'',
+                                        u'auction_type': save_doc['auction_type'],
+                                        u'mode': u'', u'api_version': None}
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document_no_auction_type], indirect=['save_doc'])
     def test_chronograph_view_auction_type(self, db2, save_doc):
-        for data in db2.view('chronograph/start_date').rows:
-            assert data.get('value') == {u'start': save_doc['stages'][0]['start'],
-                                         u'procurementMethodType': u'',
-                                         u'auction_type': 'default',
-                                         u'mode': u'', u'api_version': save_doc['TENDERS_API_VERSION']}
+        data = db2.view('chronograph/start_date').rows
+        assert len(data) == 1
+        assert data[0].get('value') == {u'start': save_doc['stages'][0]['start'],
+                                        u'procurementMethodType': u'',
+                                        u'auction_type': 'default',
+                                        u'mode': u'', u'api_version': save_doc['TENDERS_API_VERSION']}
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document_with_procur_method_type], indirect=['save_doc'])
     def test_chronograph_view_procur_method_type(self, db2, save_doc):
-        for data in db2.view('chronograph/start_date').rows:
-            assert data.get('value') == {u'start': save_doc['stages'][0]['start'],
-                                         u'procurementMethodType': save_doc['procurementMethodType'],
-                                         u'auction_type': save_doc['auction_type'],
-                                         u'mode': u'', u'api_version': save_doc['TENDERS_API_VERSION']}
+        data = db2.view('chronograph/start_date').rows
+        assert len(data) == 1
+        assert data[0].get('value') == {u'start': save_doc['stages'][0]['start'],
+                                        u'procurementMethodType': save_doc['procurementMethodType'],
+                                        u'auction_type': save_doc['auction_type'],
+                                        u'mode': u'', u'api_version': save_doc['TENDERS_API_VERSION']}
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document], indirect=['save_doc'])
     def test_start_date_view(self, db2, save_doc):
         """This test checks if view returns correct 1 stage time. Utc is ignored due couchdb javascript"""
-        for data in db2.view('auctions/by_startDate').rows:
-            result_from_couchdb = data.get('key')
-            assert data.get('value') is None
-            assert iso8601.parse_date(save_doc['stages'][0]['start']).time() == \
-                   datetime.fromtimestamp(result_from_couchdb / 1000).time()
+        data = db2.view('auctions/by_startDate').rows
+        result_from_couchdb = data[0].get('key')
+        assert len(data) == 1
+        assert data[0].get('value') is None
+        assert iso8601.parse_date(save_doc['stages'][0]['start']).time() == \
+               datetime.fromtimestamp(result_from_couchdb / 1000).time()
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document], indirect=['save_doc'])
     def test_end_date_view_1(self, db2, save_doc):
         """This test checks if view returns correct (endDate of doc) or doc.stages[0].start"""
-        for data in db2.view('auctions/by_endDate').rows:
-            result_from_couchdb = data.get('key')
-            assert data.get('value') is None
-            assert iso8601.parse_date(save_doc['stages'][0]['start']).time() == \
-                   datetime.fromtimestamp(result_from_couchdb / 1000).time()
+        data = db2.view('auctions/by_endDate').rows
+        result_from_couchdb = data[0].get('key')
+        assert len(data) == 1
+        assert data[0].get('value') is None
+        assert iso8601.parse_date(save_doc['stages'][0]['start']).time() == \
+               datetime.fromtimestamp(result_from_couchdb / 1000).time()
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document_end_date], indirect=['save_doc'])
     def test_end_date_view_2(self, db2, save_doc):
         """Here we test when couchdb returns endDate"""
-
-        for data in db2.view('auctions/by_endDate').rows:
-            result_from_couchdb = data.get('key')
-            assert data.get('value') is None
-            assert iso8601.parse_date(save_doc['endDate']).time() == \
-                   datetime.fromtimestamp(result_from_couchdb / 1000).time()
+        data = db2.view('auctions/by_endDate').rows
+        result_from_couchdb = data[0].get('key')
+        assert len(data) == 1
+        assert data[0].get('value') is None
+        assert iso8601.parse_date(save_doc['endDate']).time() == \
+               datetime.fromtimestamp(result_from_couchdb / 1000).time()
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document], indirect=['save_doc'])
     def test_pre_announce_view_1(self, db2, save_doc):
-        for data in db2.view('auctions/PreAnnounce').rows:
-            assert data.get('key') is None
-            assert data.get('value') is None
+        data = db2.view('auctions/PreAnnounce').rows
+        assert len(data) == 1
+        assert data[0].get('key') is None
+        assert data[0].get('value') is None
 
     @pytest.mark.parametrize(
         'save_doc', [test_public_document_current_stage], indirect=['save_doc'])
